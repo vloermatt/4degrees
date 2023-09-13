a8dbbffd48219828190fd459a7ea601569bbb57c
Matthew Davies <114952251+vloermatt@users.noreply.github.com>
2023-09-13 09:54:45 +0200

    fix(db): migrate to mongo

--- prisma/migrations/20230809125549_tally/migration.sql
+++
@@ -1,8 +0,0 @@
--- CreateTable
-CREATE TABLE "Tally" (
-    "id" TEXT NOT NULL PRIMARY KEY,
-    "username" TEXT NOT NULL,
-    "avatar" TEXT NOT NULL,
-    "away" INTEGER NOT NULL,
-    "home" INTEGER NOT NULL
-);
--- prisma/migrations/20230821173828_team/migration.sql
+++
@@ -1,6 +0,0 @@
--- CreateTable
-CREATE TABLE "Team" (
-    "id" TEXT NOT NULL PRIMARY KEY,
-    "name" TEXT NOT NULL,
-    "logo" TEXT NOT NULL
-);
